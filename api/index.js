f63f3361e676ed935d94dd0433d64de0d5604f94
Seemo0 <medouallal08@gmail.com>
2023-07-05 16:17:20 +0100

    sendd

---
+++ api/index.js
@@ -0,0 +1,62 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 8080;
+
+//schema
+const schemaData = mongoose.Schema(
+  {
+    name: String,
+    email: String,
+    age: Number,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("user", schemaData);
+
+app.get("/", async (req, res) => {
+  const data = await userModel.find({});
+  res.json({ success: true, data: data });
+});
+
+app.post("/create", async (req, res) => {
+  console.log(req.body);
+  const data = new userModel(req.body);
+  await data.save();
+  res.send({ success: true, message: "data save successfully", data: data });
+});
+
+app.put("/update", async (req, res) => {
+  console.log(req.body);
+  const { _id, ...rest } = req.body;
+
+  console.log(rest);
+  const data = await userModel.updateOne({ _id: _id }, rest);
+  res.send({ success: true, message: "data update successfully", data: data });
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const data = await userModel.deleteOne({ _id: id });
+  res.send({ success: true, message: "data delete successfully", data: data });
+});
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("connect to DB");
+    app.listen(PORT, () => console.log("Server is running"));
+  })
+  .catch((err) => console.log(err));
